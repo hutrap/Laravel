d870eb7fcea2a33456378da7108aebe2906a241c
hutrap <hutrap@yahoo.co.uk>
2015-06-15 12:58:50 +0100

    Add data to the table

--- Laravel/database/migrations/2015_06_15_113056_create_comments_table.php
+++ Laravel/database/migrations/2015_06_15_113056_create_comments_table.php
@@ -12,7 +12,15 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('comments', function(Blueprint $table)
+        {
+            $table->increments('id');
+
+            $table->string('text');
+            $table->string('author');
+
+            $table->timestamps();
+        });
     }
 
     /**
